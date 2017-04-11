a38ecd6d8c7c9d685496749abfb0030e39ea1218
Norbert Lanzanasto <n.lanzanasto@gmail.com>
2017-04-11 17:00:51 +0200

    Delete obsolete files.

--- src/main/java/eu/albina/model/Credentials.java
+++ src/main/java/eu/albina/model/Credentials.java
@@ -1,14 +1,11 @@
+
 package eu.albina.model;
 
 import java.io.Serializable;
 
 public class Credentials implements Serializable {
-
-	/**
-	 * 
-	 */
+	/** * */
 	private static final long serialVersionUID = 1L;
-
 	private String username;
 	private String password;
 
