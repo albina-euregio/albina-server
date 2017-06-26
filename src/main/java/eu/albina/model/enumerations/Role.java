73d580ec17e947f785cfa99cc80aa6de56711c83
Norbert Lanzanasto <n.lanzanasto@gmail.com>
2017-06-26 13:54:12 +0200

    Fix import of users.

--- src/main/java/eu/albina/model/enumerations/Role.java
+++ src/main/java/eu/albina/model/enumerations/Role.java
@@ -1,7 +1,7 @@
 package eu.albina.model.enumerations;
 
 public enum Role {
-	ADMIN, TRENTINO, SOUTH_TIROL, TIROL;
+	ADMIN, TRENTINO, SOUTH_TYROL, TYROL, VIENNA, EVTZ;
 
 	public static Role fromString(String text) {
 		if (text != null) {
