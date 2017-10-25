14923de157cf2e1ac3e9920f378745e70210d76f
Norbert Lanzanasto <n.lanzanasto@gmail.com>
2017-10-25 10:03:10 +0200

    Add CORS filter, delete header enhancer.

--- src/main/java/eu/albina/rest/filter/CorsFilter.java
+++ src/main/java/eu/albina/rest/filter/CorsFilter.java
@@ -2,22 +2,77 @@ package eu.albina.rest.filter;
 
 import java.io.IOException;
 
-import javax.ws.rs.container.ContainerRequestContext;
-import javax.ws.rs.container.ContainerResponseContext;
-import javax.ws.rs.container.ContainerResponseFilter;
-import javax.ws.rs.ext.Provider;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-@Provider
-public class CorsFilter implements ContainerResponseFilter {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-	@Override
-	public void filter(final ContainerRequestContext requestContext, final ContainerResponseContext cres)
-			throws IOException {
-		cres.getHeaders().add("Access-Control-Allow-Origin", "*");
-		cres.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
-		cres.getHeaders().add("Access-Control-Allow-Credentials", "true");
-		cres.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
-		cres.getHeaders().add("Access-Control-Max-Age", "1209600");
+/**
+ * Servlet Filter implementation class CORSFilter
+ */
+// Enable it for Servlet 3.x implementations
+/* @ WebFilter(asyncSupported = true, urlPatterns = { "/*" }) */
+public class CorsFilter implements Filter {
+
+	private static Logger logger = LoggerFactory.getLogger(CorsFilter.class);
+
+	/**
+	 * Default constructor.
+	 */
+	public CorsFilter() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain)
+			throws IOException, ServletException {
+
+		HttpServletRequest request = (HttpServletRequest) servletRequest;
+		logger.debug("CORSFilter HTTP Request: " + request.getMethod());
+
+		// Authorize (allow) all domains to consume the content
+		((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Origin", "*");
+		((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Methods",
+				"GET, OPTIONS, HEAD, PUT, POST");
+		((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Headers",
+				"origin, content-type, accept, authorization");
+		((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Credentials", "true");
+		((HttpServletResponse) servletResponse).addHeader("Access-Control-Max-Age", "1209600");
+
+		HttpServletResponse resp = (HttpServletResponse) servletResponse;
+
+		// For HTTP OPTIONS verb/method reply with ACCEPTED status code -- per CORS
+		// handshake
+		if (request.getMethod().equals("OPTIONS")) {
+			resp.setStatus(HttpServletResponse.SC_ACCEPTED);
+			return;
+		}
+
+		// pass the request along the filter chain
+		chain.doFilter(request, servletResponse);
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
 	}
 
 }
