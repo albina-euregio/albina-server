1f297af6b55a4d982964b0ea233d47367c321869
Norbert Lanzanasto <n.lanzanasto@gmail.com>
2018-09-07 07:44:50 +0200

    Delete obsolete news queries.

--- src/main/java/eu/albina/util/HibernateUtil.java
+++ src/main/java/eu/albina/util/HibernateUtil.java
@@ -20,10 +20,6 @@ public class HibernateUtil {
 	public static String queryGetSubscribersForLanguage = "from Subscriber as s where :language = s.language";
 	public static String queryGetTopLevelRegions = "from Region as r where r.parentRegion is null";
 	public static String queryGetSubregions = "from Region as r where r.parentRegion.id = :regionId";
-	public static String queryGetNews = "from News as n";
-	public static String queryGetNewsStart = "from News as n where n.datetime >= :startDate";
-	public static String queryGetNewsEnd = "from News as n where n.datetime <= :endDate";
-	public static String queryGetNewsStartEnd = "from News as n where n.datetime >= :startDate and n.datetime <= :endDate";
 	public static String queryGetChatMessages = "from ChatMessage as cm";
 	public static String queryGetChatMessagesDate = "from ChatMessage as cm where cm.dateTime >= :date";
 
