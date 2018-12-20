60bfde92e92f9f10c0dbed3de9db453a952a406d
Norbert Lanzanasto <n.lanzanasto@gmail.com>
2018-12-20 14:06:37 +0100

    Convert username to lowercase on login.

--- src/main/java/eu/albina/rest/AuthenticationService.java
+++ src/main/java/eu/albina/rest/AuthenticationService.java
@@ -40,7 +40,7 @@ public class AuthenticationService {
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_JSON)
 	public Response login(Credentials credentials) {
-		String username = credentials.getUsername();
+		String username = credentials.getUsername().toLowerCase();
 		String password = credentials.getPassword();
 
 		try {
