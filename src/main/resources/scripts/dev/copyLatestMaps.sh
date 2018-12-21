aa3757085f967bb294fa05297fdc1adf4cb252d4
Norbert Lanzanasto <n.lanzanasto@gmail.com>
2018-12-21 11:58:46 +0100

    Fix scripts for AM/PM.

--- src/main/resources/scripts/dev/copyLatestMaps.sh
+++ src/main/resources/scripts/dev/copyLatestMaps.sh
@@ -1,4 +1,8 @@
 cp /mnt/albina_files_dev/$1/*.jpg /mnt/albina_files_dev/latest/
 cp /mnt/albina_files_dev/$1/fd_overlay.png /mnt/albina_files_dev/latest/
+cp /mnt/albina_files_dev/$1/am_overlay.png /mnt/albina_files_dev/latest/
+cp /mnt/albina_files_dev/$1/pm_overlay.png /mnt/albina_files_dev/latest/
 cp /mnt/albina_files_dev/$1/fd_regions.json /mnt/albina_files_dev/latest/
+cp /mnt/albina_files_dev/$1/am_regions.json /mnt/albina_files_dev/latest/
+cp /mnt/albina_files_dev/$1/pm_regions.json /mnt/albina_files_dev/latest/
 chmod 744 /mnt/albina_files_dev/latest/*
--- src/main/resources/scripts/dev/copyMaps.sh
+++ src/main/resources/scripts/dev/copyMaps.sh
@@ -1,6 +1,14 @@
 wget -r -np -nH --cut-dirs=5 -A .jpg -P /mnt/albina_files_dev/$1/ https://data1.geo.univie.ac.at/exchange/albina2/awm_dev/$1/
 rm /mnt/albina_files_dev/$1/fd_regions.json
+rm /mnt/albina_files_dev/$1/am_regions.json
+rm /mnt/albina_files_dev/$1/pm_regions.json
 wget -nH --cut-dirs=5 -P /mnt/albina_files_dev/$1/ https://data1.geo.univie.ac.at/exchange/albina2/awm_dev/$1/fd_regions.json
+wget -nH --cut-dirs=5 -P /mnt/albina_files_dev/$1/ https://data1.geo.univie.ac.at/exchange/albina2/awm_dev/$1/am_regions.json
+wget -nH --cut-dirs=5 -P /mnt/albina_files_dev/$1/ https://data1.geo.univie.ac.at/exchange/albina2/awm_dev/$1/pm_regions.json
 rm /mnt/albina_files_dev/$1/fd_overlay.png
+rm /mnt/albina_files_dev/$1/am_overlay.png
+rm /mnt/albina_files_dev/$1/pm_overlay.png
 wget -nH --cut-dirs=5 -P /mnt/albina_files_dev/$1/ https://data1.geo.univie.ac.at/exchange/albina2/awm_dev/$1/fd_overlay.png
+wget -nH --cut-dirs=5 -P /mnt/albina_files_dev/$1/ https://data1.geo.univie.ac.at/exchange/albina2/awm_dev/$1/am_overlay.png
+wget -nH --cut-dirs=5 -P /mnt/albina_files_dev/$1/ https://data1.geo.univie.ac.at/exchange/albina2/awm_dev/$1/pm_overlay.png
 chmod 744 /mnt/albina_files_dev/$1/*
