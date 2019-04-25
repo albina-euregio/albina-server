e8c6eefbd6a9314eb95bdc287d9511c50fac630f
Norbert Lanzanasto <n.lanzanasto@gmail.com>
2019-04-25 07:31:04 +0200

    Add header comments.

--- src/main/java/eu/albina/model/rapidmail/recipients/post/PostRecipientsResponse.java
+++ src/main/java/eu/albina/model/rapidmail/recipients/post/PostRecipientsResponse.java
@@ -1,7 +1,24 @@
+/*******************************************************************************
+ * Copyright (C) 2019 Clesius srl
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
 package eu.albina.model.rapidmail.recipients.post;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import javax.annotation.Generated;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 @Generated("com.robohorse.robopojogenerator")
 public class PostRecipientsResponse {
@@ -117,343 +134,317 @@ public class PostRecipientsResponse {
 	@JsonProperty("status")
 	private String status;
 
-	public void setMailtype(String mailtype){
+	public void setMailtype(String mailtype) {
 		this.mailtype = mailtype;
 	}
 
-	public String getMailtype(){
+	public String getMailtype() {
 		return mailtype;
 	}
 
-	public void setFirstname(String firstname){
+	public void setFirstname(String firstname) {
 		this.firstname = firstname;
 	}
 
-	public String getFirstname(){
+	public String getFirstname() {
 		return firstname;
 	}
 
-	public void setExtra8(String extra8){
+	public void setExtra8(String extra8) {
 		this.extra8 = extra8;
 	}
 
-	public String getExtra8(){
+	public String getExtra8() {
 		return extra8;
 	}
 
-	public void setBirthdate(String birthdate){
+	public void setBirthdate(String birthdate) {
 		this.birthdate = birthdate;
 	}
 
-	public String getBirthdate(){
+	public String getBirthdate() {
 		return birthdate;
 	}
 
-	public void setExtra9(String extra9){
+	public void setExtra9(String extra9) {
 		this.extra9 = extra9;
 	}
 
-	public String getExtra9(){
+	public String getExtra9() {
 		return extra9;
 	}
 
-	public void setGender(String gender){
+	public void setGender(String gender) {
 		this.gender = gender;
 	}
 
-	public String getGender(){
+	public String getGender() {
 		return gender;
 	}
 
-	public void setExtra2(String extra2){
+	public void setExtra2(String extra2) {
 		this.extra2 = extra2;
 	}
 
-	public String getExtra2(){
+	public String getExtra2() {
 		return extra2;
 	}
 
-	public void setExtra3(String extra3){
+	public void setExtra3(String extra3) {
 		this.extra3 = extra3;
 	}
 
-	public String getExtra3(){
+	public String getExtra3() {
 		return extra3;
 	}
 
-	public void setExtra1(String extra1){
+	public void setExtra1(String extra1) {
 		this.extra1 = extra1;
 	}
 
-	public String getExtra1(){
+	public String getExtra1() {
 		return extra1;
 	}
 
-	public void setTitle(String title){
+	public void setTitle(String title) {
 		this.title = title;
 	}
 
-	public String getTitle(){
+	public String getTitle() {
 		return title;
 	}
 
-	public void setExtra6(String extra6){
+	public void setExtra6(String extra6) {
 		this.extra6 = extra6;
 	}
 
-	public String getExtra6(){
+	public String getExtra6() {
 		return extra6;
 	}
 
-	public void setExtra7(String extra7){
+	public void setExtra7(String extra7) {
 		this.extra7 = extra7;
 	}
 
-	public String getExtra7(){
+	public String getExtra7() {
 		return extra7;
 	}
 
-	public void setExtra4(String extra4){
+	public void setExtra4(String extra4) {
 		this.extra4 = extra4;
 	}
 
-	public String getExtra4(){
+	public String getExtra4() {
 		return extra4;
 	}
 
-	public void setExtra5(String extra5){
+	public void setExtra5(String extra5) {
 		this.extra5 = extra5;
 	}
 
-	public String getExtra5(){
+	public String getExtra5() {
 		return extra5;
 	}
 
-	public void setId(int id){
+	public void setId(int id) {
 		this.id = id;
 	}
 
-	public int getId(){
+	public int getId() {
 		return id;
 	}
 
-	public void setExtra10(String extra10){
+	public void setExtra10(String extra10) {
 		this.extra10 = extra10;
 	}
 
-	public String getExtra10(){
+	public String getExtra10() {
 		return extra10;
 	}
 
-	public void setEmail(String email){
+	public void setEmail(String email) {
 		this.email = email;
 	}
 
-	public String getEmail(){
+	public String getEmail() {
 		return email;
 	}
 
-	public void setCreatedHostname(String createdHostname){
+	public void setCreatedHostname(String createdHostname) {
 		this.createdHostname = createdHostname;
 	}
 
-	public String getCreatedHostname(){
+	public String getCreatedHostname() {
 		return createdHostname;
 	}
 
-	public void setZip(int zip){
+	public void setZip(int zip) {
 		this.zip = zip;
 	}
 
-	public int getZip(){
+	public int getZip() {
 		return zip;
 	}
 
-	public void setExtrabig9(String extrabig9){
+	public void setExtrabig9(String extrabig9) {
 		this.extrabig9 = extrabig9;
 	}
 
-	public String getExtrabig9(){
+	public String getExtrabig9() {
 		return extrabig9;
 	}
 
-	public void setExtrabig8(String extrabig8){
+	public void setExtrabig8(String extrabig8) {
 		this.extrabig8 = extrabig8;
 	}
 
-	public String getExtrabig8(){
+	public String getExtrabig8() {
 		return extrabig8;
 	}
 
-	public void setCreated(String created){
+	public void setCreated(String created) {
 		this.created = created;
 	}
 
-	public String getCreated(){
+	public String getCreated() {
 		return created;
 	}
 
-	public void setExtrabig5(String extrabig5){
+	public void setExtrabig5(String extrabig5) {
 		this.extrabig5 = extrabig5;
 	}
 
-	public String getExtrabig5(){
+	public String getExtrabig5() {
 		return extrabig5;
 	}
 
-	public void setExtrabig4(String extrabig4){
+	public void setExtrabig4(String extrabig4) {
 		this.extrabig4 = extrabig4;
 	}
 
-	public String getExtrabig4(){
+	public String getExtrabig4() {
 		return extrabig4;
 	}
 
-	public void setExtrabig7(String extrabig7){
+	public void setExtrabig7(String extrabig7) {
 		this.extrabig7 = extrabig7;
 	}
 
-	public String getExtrabig7(){
+	public String getExtrabig7() {
 		return extrabig7;
 	}
 
-	public void setExtrabig6(String extrabig6){
+	public void setExtrabig6(String extrabig6) {
 		this.extrabig6 = extrabig6;
 	}
 
-	public String getExtrabig6(){
+	public String getExtrabig6() {
 		return extrabig6;
 	}
 
-	public void setExtrabig1(String extrabig1){
+	public void setExtrabig1(String extrabig1) {
 		this.extrabig1 = extrabig1;
 	}
 
-	public String getExtrabig1(){
+	public String getExtrabig1() {
 		return extrabig1;
 	}
 
-	public void setExtrabig3(String extrabig3){
+	public void setExtrabig3(String extrabig3) {
 		this.extrabig3 = extrabig3;
 	}
 
-	public String getExtrabig3(){
+	public String getExtrabig3() {
 		return extrabig3;
 	}
 
-	public void setLastname(String lastname){
+	public void setLastname(String lastname) {
 		this.lastname = lastname;
 	}
 
-	public String getLastname(){
+	public String getLastname() {
 		return lastname;
 	}
 
-	public void setExtrabig2(String extrabig2){
+	public void setExtrabig2(String extrabig2) {
 		this.extrabig2 = extrabig2;
 	}
 
-	public String getExtrabig2(){
+	public String getExtrabig2() {
 		return extrabig2;
 	}
 
-	public void setForeignId(int foreignId){
+	public void setForeignId(int foreignId) {
 		this.foreignId = foreignId;
 	}
 
-	public int getForeignId(){
+	public int getForeignId() {
 		return foreignId;
 	}
 
-	public void setRecipientlistId(int recipientlistId){
+	public void setRecipientlistId(int recipientlistId) {
 		this.recipientlistId = recipientlistId;
 	}
 
-	public int getRecipientlistId(){
+	public int getRecipientlistId() {
 		return recipientlistId;
 	}
 
-	public void setExtrabig10(String extrabig10){
+	public void setExtrabig10(String extrabig10) {
 		this.extrabig10 = extrabig10;
 	}
 
-	public String getExtrabig10(){
+	public String getExtrabig10() {
 		return extrabig10;
 	}
 
-	public void setUpdated(String updated){
+	public void setUpdated(String updated) {
 		this.updated = updated;
 	}
 
-	public String getUpdated(){
+	public String getUpdated() {
 		return updated;
 	}
 
-	public void setCreatedIp(String createdIp){
+	public void setCreatedIp(String createdIp) {
 		this.createdIp = createdIp;
 	}
 
-	public String getCreatedIp(){
+	public String getCreatedIp() {
 		return createdIp;
 	}
 
-	public void setActivated(String activated){
+	public void setActivated(String activated) {
 		this.activated = activated;
 	}
 
-	public String getActivated(){
+	public String getActivated() {
 		return activated;
 	}
 
-	public void setStatus(String status){
+	public void setStatus(String status) {
 		this.status = status;
 	}
 
-	public String getStatus(){
+	public String getStatus() {
 		return status;
 	}
 
 	@Override
- 	public String toString(){
-		return 
-			"RecipientsResponse{" + 
-			"mailtype = '" + mailtype + '\'' + 
-			",firstname = '" + firstname + '\'' + 
-			",extra8 = '" + extra8 + '\'' + 
-			",birthdate = '" + birthdate + '\'' + 
-			",extra9 = '" + extra9 + '\'' + 
-			",gender = '" + gender + '\'' + 
-			",extra2 = '" + extra2 + '\'' + 
-			",extra3 = '" + extra3 + '\'' + 
-			",extra1 = '" + extra1 + '\'' + 
-			",title = '" + title + '\'' + 
-			",extra6 = '" + extra6 + '\'' + 
-			",extra7 = '" + extra7 + '\'' + 
-			",extra4 = '" + extra4 + '\'' + 
-			",extra5 = '" + extra5 + '\'' + 
-			",id = '" + id + '\'' + 
-			",extra10 = '" + extra10 + '\'' + 
-			",email = '" + email + '\'' + 
-			",created_hostname = '" + createdHostname + '\'' + 
-			",zip = '" + zip + '\'' + 
-			",extrabig9 = '" + extrabig9 + '\'' + 
-			",extrabig8 = '" + extrabig8 + '\'' + 
-			",created = '" + created + '\'' + 
-			",extrabig5 = '" + extrabig5 + '\'' + 
-			",extrabig4 = '" + extrabig4 + '\'' + 
-			",extrabig7 = '" + extrabig7 + '\'' + 
-			",extrabig6 = '" + extrabig6 + '\'' + 
-			",extrabig1 = '" + extrabig1 + '\'' + 
-			",extrabig3 = '" + extrabig3 + '\'' + 
-			",lastname = '" + lastname + '\'' + 
-			",extrabig2 = '" + extrabig2 + '\'' + 
-			",foreign_id = '" + foreignId + '\'' + 
-			",recipientlist_id = '" + recipientlistId + '\'' + 
-			",extrabig10 = '" + extrabig10 + '\'' + 
-			",updated = '" + updated + '\'' + 
-			",created_ip = '" + createdIp + '\'' + 
-			",activated = '" + activated + '\'' + 
-			",status = '" + status + '\'' + 
-			"}";
-		}
+	public String toString() {
+		return "RecipientsResponse{" + "mailtype = '" + mailtype + '\'' + ",firstname = '" + firstname + '\''
+				+ ",extra8 = '" + extra8 + '\'' + ",birthdate = '" + birthdate + '\'' + ",extra9 = '" + extra9 + '\''
+				+ ",gender = '" + gender + '\'' + ",extra2 = '" + extra2 + '\'' + ",extra3 = '" + extra3 + '\''
+				+ ",extra1 = '" + extra1 + '\'' + ",title = '" + title + '\'' + ",extra6 = '" + extra6 + '\''
+				+ ",extra7 = '" + extra7 + '\'' + ",extra4 = '" + extra4 + '\'' + ",extra5 = '" + extra5 + '\''
+				+ ",id = '" + id + '\'' + ",extra10 = '" + extra10 + '\'' + ",email = '" + email + '\''
+				+ ",created_hostname = '" + createdHostname + '\'' + ",zip = '" + zip + '\'' + ",extrabig9 = '"
+				+ extrabig9 + '\'' + ",extrabig8 = '" + extrabig8 + '\'' + ",created = '" + created + '\''
+				+ ",extrabig5 = '" + extrabig5 + '\'' + ",extrabig4 = '" + extrabig4 + '\'' + ",extrabig7 = '"
+				+ extrabig7 + '\'' + ",extrabig6 = '" + extrabig6 + '\'' + ",extrabig1 = '" + extrabig1 + '\''
+				+ ",extrabig3 = '" + extrabig3 + '\'' + ",lastname = '" + lastname + '\'' + ",extrabig2 = '" + extrabig2
+				+ '\'' + ",foreign_id = '" + foreignId + '\'' + ",recipientlist_id = '" + recipientlistId + '\''
+				+ ",extrabig10 = '" + extrabig10 + '\'' + ",updated = '" + updated + '\'' + ",created_ip = '"
+				+ createdIp + '\'' + ",activated = '" + activated + '\'' + ",status = '" + status + '\'' + "}";
+	}
 }
