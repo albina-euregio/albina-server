1b7920472ec4bb87f6f9a7d3a13eb65f430e0c13
Norbert Lanzanasto <n.lanzanasto@gmail.com>
2019-04-26 08:09:50 +0200

    Add Clesius srl as author to social media controllers.

--- src/main/java/eu/albina/controller/socialmedia/TwitterProcessorController.java
+++ src/main/java/eu/albina/controller/socialmedia/TwitterProcessorController.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (C) 2019 Norbert Lanzanasto
+ * Copyright (C) 2019 Clesius srl
  * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
