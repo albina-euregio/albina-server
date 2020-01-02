752922381837443400a77e08849378bee45d30bc
Simon Legner <Simon.Legner@gmail.com>
2020-01-02 19:12:45 +0100

    style: re-indent using tabs

--- src/test/java/eu/albina/model/enumerations/BulletinStatusTest.java
+++ src/test/java/eu/albina/model/enumerations/BulletinStatusTest.java
@@ -9,15 +9,15 @@ import java.util.List;
 import static org.junit.Assert.*;
 
 public class BulletinStatusTest {
-    @Test
-    public void testCompare() {
-        final List<BulletinStatus> expected = Arrays.asList(BulletinStatus.missing, BulletinStatus.draft, BulletinStatus.updated, BulletinStatus.submitted, BulletinStatus.resubmitted, BulletinStatus.published, BulletinStatus.republished);
-        for (int i = 0; i < 10; i++) {
-            final List<BulletinStatus> statusList = Arrays.asList(BulletinStatus.values());
-            Collections.shuffle(statusList);
-            statusList.sort(BulletinStatus::comparePublicationStatus);
-            assertEquals(expected, statusList);
-        }
+	@Test
+	public void testCompare() {
+		final List<BulletinStatus> expected = Arrays.asList(BulletinStatus.missing, BulletinStatus.draft, BulletinStatus.updated, BulletinStatus.submitted, BulletinStatus.resubmitted, BulletinStatus.published, BulletinStatus.republished);
+		for (int i = 0; i < 10; i++) {
+			final List<BulletinStatus> statusList = Arrays.asList(BulletinStatus.values());
+			Collections.shuffle(statusList);
+			statusList.sort(BulletinStatus::comparePublicationStatus);
+			assertEquals(expected, statusList);
+		}
 
-    }
+	}
 }
