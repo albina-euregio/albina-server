d95e472b176a1a55542953b730e2cdd2d12ee580
Simon Legner <Simon.Legner@gmail.com>
2020-01-02 19:52:49 +0100

    refact(JsonValidator): use Resources.getResource

--- src/main/java/eu/albina/util/GlobalVariables.java
+++ src/main/java/eu/albina/util/GlobalVariables.java
@@ -1,27 +1,23 @@
 /*******************************************************************************
  * Copyright (C) 2019 Norbert Lanzanasto
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  ******************************************************************************/
 package eu.albina.util;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 import org.apache.commons.configuration2.Configuration;
 import org.apache.commons.configuration2.PropertiesConfiguration;
@@ -164,7 +160,7 @@ public class GlobalVariables {
 	// REGION
 	public static List<String> regions = new ArrayList<String>() {
 		/**
-		 * 
+		 *
 		 */
 		private static final long serialVersionUID = 1L;
 
@@ -178,7 +174,7 @@ public class GlobalVariables {
 	// REGION
 	public static List<String> regionsEuregio = new ArrayList<String>() {
 		/**
-		 * 
+		 *
 		 */
 		private static final long serialVersionUID = 1L;
 
@@ -192,7 +188,7 @@ public class GlobalVariables {
 	// LANG
 	public static List<LanguageCode> languages = new ArrayList<LanguageCode>() {
 		/**
-		 * 
+		 *
 		 */
 		private static final long serialVersionUID = 1L;
 
@@ -505,22 +501,6 @@ public class GlobalVariables {
 	public static String getMapsUrl(LanguageCode lang) {
 		return GlobalVariables.getAvalancheReportBaseUrl(lang)
 				+ GlobalVariables.getMapsPath().substring(GlobalVariables.directoryOffset);
-	}
-
-	public static String getJsonSchemaFileString(String fileName) throws FileNotFoundException {
-		StringBuilder result = new StringBuilder("");
-
-		URL resource = GlobalVariables.class.getResource("/" + fileName + ".json");
-		File file = new File(resource.getFile());
-
-		Scanner scanner = new Scanner(file);
-		while (scanner.hasNextLine()) {
-			String line = scanner.nextLine();
-			result.append(line).append("\n");
-		}
-		scanner.close();
-
-		return result.toString();
 	}
 
 	// LANG
