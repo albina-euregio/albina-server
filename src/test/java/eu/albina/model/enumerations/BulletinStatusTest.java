e69f877bc560ccc0bd9e2cff753f55a089a48814
Simon Legner <Simon.Legner@gmail.com>
2020-02-12 13:53:33 +0100

    Revert "fix: bulletin status test"
    This reverts commit aa01d4a229d98574ce8ec1d95fad0caefda059a2.

--- src/test/java/eu/albina/model/enumerations/BulletinStatusTest.java
+++ src/test/java/eu/albina/model/enumerations/BulletinStatusTest.java
@@ -1,19 +1,17 @@
 package eu.albina.model.enumerations;
 
-import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class BulletinStatusTest {
 	@Test
 	public void testCompare() {
-		final List<BulletinStatus> expected = Arrays.asList(BulletinStatus.missing, BulletinStatus.draft,
-				BulletinStatus.submitted, BulletinStatus.published, BulletinStatus.updated, BulletinStatus.resubmitted,
-				BulletinStatus.republished);
+		final List<BulletinStatus> expected = Arrays.asList(BulletinStatus.missing, BulletinStatus.draft, BulletinStatus.updated, BulletinStatus.submitted, BulletinStatus.resubmitted, BulletinStatus.published, BulletinStatus.republished);
 		for (int i = 0; i < 10; i++) {
 			final List<BulletinStatus> statusList = Arrays.asList(BulletinStatus.values());
 			Collections.shuffle(statusList);
