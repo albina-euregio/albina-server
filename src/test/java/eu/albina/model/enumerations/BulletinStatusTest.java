aa01d4a229d98574ce8ec1d95fad0caefda059a2
Norbert Lanzanasto <n.lanzanasto@gmail.com>
2020-02-12 13:29:28 +0100

    fix: bulletin status test

--- src/test/java/eu/albina/model/enumerations/BulletinStatusTest.java
+++ src/test/java/eu/albina/model/enumerations/BulletinStatusTest.java
@@ -1,17 +1,19 @@
 package eu.albina.model.enumerations;
 
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-import static org.junit.Assert.*;
+import org.junit.Test;
 
 public class BulletinStatusTest {
 	@Test
 	public void testCompare() {
-		final List<BulletinStatus> expected = Arrays.asList(BulletinStatus.missing, BulletinStatus.draft, BulletinStatus.updated, BulletinStatus.submitted, BulletinStatus.resubmitted, BulletinStatus.published, BulletinStatus.republished);
+		final List<BulletinStatus> expected = Arrays.asList(BulletinStatus.missing, BulletinStatus.draft,
+				BulletinStatus.submitted, BulletinStatus.published, BulletinStatus.updated, BulletinStatus.resubmitted,
+				BulletinStatus.republished);
 		for (int i = 0; i < 10; i++) {
 			final List<BulletinStatus> statusList = Arrays.asList(BulletinStatus.values());
 			Collections.shuffle(statusList);
