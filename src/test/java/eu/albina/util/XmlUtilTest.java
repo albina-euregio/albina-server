45047c26980b90a7d3ff25330c8a101db28189d1
Simon Legner <Simon.Legner@gmail.com>
2020-07-30 12:24:13 +0200

    feat: XmlUtilTest.createOldCaamlFiles

--- src/test/java/eu/albina/util/XmlUtilTest.java
+++ src/test/java/eu/albina/util/XmlUtilTest.java
@@ -2,11 +2,18 @@ package eu.albina.util;
 
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
+import eu.albina.controller.AvalancheReportController;
+import org.joda.time.DateTimeZone;
+import org.joda.time.LocalDate;
 import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
+import org.slf4j.LoggerFactory;
 import org.w3c.dom.Document;
 
 import com.google.common.io.Resources;
@@ -55,5 +62,22 @@ public class XmlUtilTest {
 				.toString(Resources.getResource("2019-01-16.caaml.v6.xml"), StandardCharsets.UTF_8).replace("\t", "  ");
 		final String xml = createCaaml(CaamlVersion.V6);
 		Assert.assertEquals(expected, xml);
+	}
+
+	@Ignore
+	@Test
+	public void createOldCaamlFiles() throws Exception {
+		HibernateUtil.getInstance().setUp();
+		for (LocalDate date = new LocalDate("2019-11-16"); date.isBefore(new LocalDate("2020-05-04")); date = date.plusDays(1)) {
+			List<AvalancheBulletin> result = AvalancheReportController.getInstance()
+				.getPublishedBulletins(date.toDateTimeAtStartOfDay().withZone(DateTimeZone.UTC), GlobalVariables.regions);
+			for (LanguageCode language : GlobalVariables.languages) {
+				Path path = Paths.get("/tmp/albina_files" + "/" + date + "/" + date + "_" + language + "_CAAMLv6.xml");
+				Document caamlDoc = XmlUtil.createCaaml(result, language, CaamlVersion.V6);
+				String caaml = XmlUtil.convertDocToString(caamlDoc);
+				LoggerFactory.getLogger(getClass()).info("Writing {}", path);
+				Files.write(path, caaml.getBytes(StandardCharsets.UTF_8));
+			}
+		}
 	}
 }
