f95a4fd211c661f9ebc3a627aaeb0a6432a7eed2
Simon Legner <Simon.Legner@gmail.com>
2020-09-30 10:13:25 +0200

    fix: DangerPatternTest

--- src/test/java/eu/albina/model/enumerations/DangerPatternTest.java
+++ src/test/java/eu/albina/model/enumerations/DangerPatternTest.java
@@ -9,6 +9,7 @@ public class DangerPatternTest {
 	@Test
 	public void testI18n() {
 		assertEquals("gm 7: schneearm neben schneereich", DangerPattern.dp7.toString(Locale.GERMAN));
-		assertEquals("dp 7: snow-poor zones in snow-rich surrounding", DangerPattern.dp7.toString(Locale.FRENCH)); // until fr is translated
+		assertEquals("md7: zones avec peu de neige \u00E0 cot\u00E9 des zones enneig\u00E9es",
+			DangerPattern.dp7.toString(Locale.FRENCH));
 	}
 }
