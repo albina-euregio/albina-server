7315ea2f474afda5e3ce6ce308d9aa660c88e567
Norbert Lanzanasto <n.lanzanasto@gmail.com>
2020-11-11 17:58:48 +0100

    fix(test): enumerations test

--- src/test/java/eu/albina/model/enumerations/DangerPatternTest.java
+++ src/test/java/eu/albina/model/enumerations/DangerPatternTest.java
@@ -1,15 +1,16 @@
 package eu.albina.model.enumerations;
 
+import static org.junit.Assert.assertEquals;
+
 import java.util.Locale;
 
-import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class DangerPatternTest {
 	@Test
 	public void testI18n() {
-		assertEquals("gm 7: schneearm neben schneereich", DangerPattern.dp7.toString(Locale.GERMAN));
-		assertEquals("md 7: zones avec peu de neige à coté des zones enneigées",
-			DangerPattern.dp7.toString(Locale.FRENCH));
+		assertEquals("gm.7: schneearm neben schneereich", DangerPattern.dp7.toString(Locale.GERMAN));
+		assertEquals("md.7: zones avec peu de neige à coté des zones enneigées",
+				DangerPattern.dp7.toString(Locale.FRENCH));
 	}
 }
