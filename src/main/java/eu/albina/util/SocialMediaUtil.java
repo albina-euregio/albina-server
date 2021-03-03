a2df5d986127773fd9153542c8335621c31e66fe
Simon Legner <Simon.Legner@gmail.com>
2021-03-03 17:18:29 +0100

    chore(SocialMediaUtil): use fd_albina_thumbnail.jpg

--- src/main/java/eu/albina/util/SocialMediaUtil.java
+++ src/main/java/eu/albina/util/SocialMediaUtil.java
@@ -52,7 +52,7 @@ interface SocialMediaUtil {
 		String publicationTime = AlbinaUtil.getPublicationTime(bulletins);
 		return GlobalVariables.getServerMainUrl() + GlobalVariables.avalancheReportFilesUrl
 			+ validityDate + "/" + publicationTime + "/"
-			+ AlbinaUtil.getRegionOverviewMapFilename("", "jpg");
+			+ AlbinaUtil.getRegionOverviewMapFilename("", "jpg").replace("map.jpg", "thumbnail.jpg");
 	}
 
 	void sendBulletinNewsletter(String message, LanguageCode lang, List<String> regions, String attachmentUrl);
