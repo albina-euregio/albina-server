27b81f85844059aa06ddd819a271766a6a1f3858
Simon Legner <Simon.Legner@gmail.com>
2021-03-03 19:22:33 +0100

    Revert "refact(SimpleHtmlUtilTest): assert in unit test"
    This reverts commit faec7923

--- src/test/java/eu/albina/util/SimpleHtmlUtilTest.java
+++ src/test/java/eu/albina/util/SimpleHtmlUtilTest.java
@@ -18,12 +18,10 @@ package eu.albina.util;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
@@ -68,8 +66,7 @@ public class SimpleHtmlUtilTest {
 
 	@Test
 	public void createSimpleHtmlFreemarker() throws IOException, URISyntaxException, TemplateException {
-		String expected = Resources.toString(Resources.getResource("2030-02-16.simple.html"), StandardCharsets.UTF_8).trim();
 		String htmlString = SimpleHtmlUtil.getInstance().createSimpleHtmlString(bulletins, LanguageCode.de, "");
-		Assert.assertEquals(expected, htmlString);
+		System.out.println(htmlString);
 	}
 }
