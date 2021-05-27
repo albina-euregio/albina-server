7e30463a858ae1a13125f765832913c4c9aac211
Norbert Lanzanasto <n.lanzanasto@gmail.com>
2021-05-27 11:55:10 +0200

    chore(test): enable region controller tests

--- src/test/java/eu/albina/controller/RegionControllerTest.java
+++ src/test/java/eu/albina/controller/RegionControllerTest.java
@@ -21,7 +21,6 @@ import java.util.List;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import eu.albina.exception.AlbinaException;
@@ -44,14 +43,12 @@ public class RegionControllerTest {
 		HibernateUtil.getInstance().shutDown();
 	}
 
-	@Ignore
 	@Test
 	public void getTopLevelRegionsTest() throws AlbinaException {
 		List<Region> regions = RegionController.getInstance().getRegions("");
-		Assert.assertEquals(6, regions.size());
+		Assert.assertEquals(4, regions.size());
 	}
 
-	@Ignore
 	@Test
 	public void getSubregionsTest() throws AlbinaException {
 		List<Region> regions = RegionController.getInstance().getRegions(GlobalVariables.codeTrentino);
