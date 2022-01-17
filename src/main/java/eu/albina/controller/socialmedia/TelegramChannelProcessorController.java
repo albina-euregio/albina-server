875d78a880fa1b3c82d3eb649e35e8990cc9a8a2
Simon Legner <Simon.Legner@gmail.com>
2022-01-17 21:56:19 +0100

    refact(TelegramChannelProcessorController): JAX-RS Client
    https://www.baeldung.com/jersey-jax-rs-client

--- src/main/java/eu/albina/controller/socialmedia/TelegramChannelProcessorController.java
+++ src/main/java/eu/albina/controller/socialmedia/TelegramChannelProcessorController.java
@@ -17,26 +17,24 @@
 package eu.albina.controller.socialmedia;
 
 import java.io.IOException;
-import java.net.URI;
 import java.net.URISyntaxException;
-import java.net.URLEncoder;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.fluent.Request;
-import org.apache.http.client.utils.URIBuilder;
-import org.apache.http.util.EntityUtils;
+import eu.albina.util.HttpClientUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import eu.albina.model.socialmedia.TelegramConfig;
 
-public class TelegramChannelProcessorController extends CommonProcessor {
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Response;
+
+public class TelegramChannelProcessorController {
 	private static final Logger logger = LoggerFactory.getLogger(TelegramChannelProcessorController.class);
 
 	private static TelegramChannelProcessorController instance = null;
-	private final int CONNECTION_TIMEOUT = 10000;
-	private final int SOCKET_TIMEOUT = 10000;
+
+	private final Client client = HttpClientUtil.newClientBuilder().build();
 
 	public static TelegramChannelProcessorController getInstance() {
 		if (instance == null) {
@@ -48,75 +46,33 @@ public class TelegramChannelProcessorController extends CommonProcessor {
 	public TelegramChannelProcessorController() {
 	}
 
-	public HttpResponse sendPhoto(TelegramConfig config, String message, String attachmentUrl, boolean test)
+	public Response sendPhoto(TelegramConfig config, String message, String attachmentUrl, boolean test)
 			throws IOException, URISyntaxException {
-		String chatId = test ? "aws_test" : config.getChatId();
-
-		URIBuilder uriBuilder = new URIBuilder(
-				String.format("https://api.telegram.org/bot%s/sendPhoto", config.getApiToken()))
-						.addParameter("chat_id", chatId).addParameter("caption", message);
-		if (attachmentUrl != null) {
-			uriBuilder.addParameter("photo", attachmentUrl);
-		}
-		URI uri = uriBuilder.build();
-		logger.info("URL: {}", uri);
-		Request request = Request.Get(uri).connectTimeout(CONNECTION_TIMEOUT).socketTimeout(SOCKET_TIMEOUT);
-		HttpResponse response = request.execute().returnResponse();
-
-		// Go ahead only if success
-		if (response.getStatusLine().getStatusCode() != 200) {
-			logger.warn("Error publishing photo on telegram channel for "
-					+ config.getRegionConfiguration().getRegion().getId() + " (error code "
-					+ response.getStatusLine().getStatusCode() + ")");
-			HttpEntity entity = response.getEntity();
-			String content = EntityUtils.toString(entity);
-			logger.warn(content);
-		}
-
-		return response;
+		// https://core.telegram.org/bots/api#sendphoto
+		String chatId = test ? "@aws_test" : config.getChatId();
+		WebTarget request = client.target(String.format("https://api.telegram.org/bot%s/sendPhoto", config.getApiToken()))
+			.queryParam("chat_id", chatId)
+			.queryParam("caption", message)
+			.queryParam("photo", attachmentUrl);
+		return execute(request, config);
 	}
 
-	public HttpResponse sendMessage(TelegramConfig config, String message, boolean test) throws IOException, URISyntaxException {
-		String chatId = test ? "aws_test" : config.getChatId();
-		URIBuilder uriBuilder = new URIBuilder(
-			String.format("https://api.telegram.org/bot%s/sendMessage", config.getApiToken()))
-					.addParameter("chat_id", chatId).addParameter("text", message);
-		URI uri = uriBuilder.build();
-		logger.info("URL: {}", uri);
-		Request request = Request.Get(uri).connectTimeout(CONNECTION_TIMEOUT).socketTimeout(SOCKET_TIMEOUT);
-		HttpResponse response = request.execute().returnResponse();
-
-		// Go ahead only if success
-		if (response.getStatusLine().getStatusCode() != 200) {
-			logger.warn("Error publishing message on telegram channel for "
-					+ config.getRegionConfiguration().getRegion().getId() + " (error code "
-					+ response.getStatusLine().getStatusCode() + ")");
-			HttpEntity entity = response.getEntity();
-			String content = EntityUtils.toString(entity);
-			logger.warn(content);
-		}
-
-		return response;
+	public Response sendMessage(TelegramConfig config, String message, boolean test) throws IOException, URISyntaxException {
+		// https://core.telegram.org/bots/api#sendmessage
+		String chatId = test ? "@aws_test" : config.getChatId();
+		WebTarget request = client.target(String.format("https://api.telegram.org/bot%s/sendMessage", config.getApiToken()))
+			.queryParam("chat_id", chatId)
+			.queryParam("text", message);
+		return execute(request, config);
 	}
 
-	public HttpResponse sendFile(TelegramConfig config, String message, String attachmentUrl, boolean test) throws IOException {
-		String urlString = "https://api.telegram.org/bot%s/sendDocument?chat_id=%s&caption=%s&document=%s";
-
-		String chatId = test ? "aws_test" : config.getChatId();
-
-		urlString = String.format(urlString, config.getApiToken(), chatId,
-				URLEncoder.encode(message, "UTF-8"), URLEncoder.encode(attachmentUrl, "UTF-8"));
-
-		Request request = Request.Get(urlString).connectTimeout(CONNECTION_TIMEOUT).socketTimeout(SOCKET_TIMEOUT);
-		HttpResponse response = request.execute().returnResponse();
-
-		// Go ahead only if success
-		if (response.getStatusLine().getStatusCode() != 200) {
-			logger.warn("Error publishing report on telegram channel for "
-					+ config.getRegionConfiguration().getRegion().getId() + " (error code "
-					+ response.getStatusLine().getStatusCode() + ")");
+	private Response execute(WebTarget request, TelegramConfig config) {
+		final Response response = request.request().get();
+		if (response.getStatusInfo().getStatusCode() != 200) {
+			logger.warn("Error publishing on telegram channel for "
+				+ config.getRegionConfiguration().getRegion().getId() + " (error code "
+				+ response.getStatusInfo().getStatusCode() + "): " + response.readEntity(String.class));
 		}
-
 		return response;
 	}
 }
