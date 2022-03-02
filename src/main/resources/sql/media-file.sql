c2ced397dfdb0b2f2e1a1454b258b28ed3f93e12
Norbert Lanzanasto <n.lanzanasto@gmail.com>
2022-03-02 18:07:39 +0100

    chore(sql): add script for media file columns in db

--- src/main/resources/sql/media-file.sql
+++ src/main/resources/sql/media-file.sql
@@ -1,3 +1,6 @@
 /* Add column to avalanche reports */
 ALTER TABLE `ais`.`avalanche_reports`
 ADD `MEDIA_FILE_UPLOADED` bit DEFAULT FALSE
+
+ALTER TABLE `ais`.`server_instances`
+ADD `MEDIA_PATH` varchar(255);
