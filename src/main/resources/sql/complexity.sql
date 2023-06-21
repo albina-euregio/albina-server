5cb114046f0746fc77dcc5ed9457677b783da412
G Weiss <weiss@slf.ch>
2023-06-21 08:11:32 +0200

    chore(db): delete old database scripts
    Relates to: #274

--- src/main/resources/sql/complexity.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `ais`.`avalanche_bulletin_daytime_descriptions`
-ADD `COMPLEXITY` varchar(255) DEFAULT NULL
--- src/main/resources/sql/terrain-feature.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE `ais`.`avalanche_bulletin_daytime_descriptions`
-ADD `TERRAIN_FEATURE_ABOVE_TEXTCAT` longtext DEFAULT NULL,
-ADD `TERRAIN_FEATURE_BELOW_TEXTCAT` longtext DEFAULT NULL
