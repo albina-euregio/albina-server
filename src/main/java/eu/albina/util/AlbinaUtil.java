63feb485e12b63437bf3f1eedff14df79f0add1b
Simon Legner <Simon.Legner@gmail.com>
2024-10-31 14:38:50 +0100

    refact(PdfUtil): move methods

--- src/main/java/eu/albina/util/AlbinaUtil.java
+++ src/main/java/eu/albina/util/AlbinaUtil.java
@@ -32,15 +32,12 @@ import java.time.Period;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
-import java.text.MessageFormat;
 import java.time.temporal.ChronoUnit;
 import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Locale;
 import java.util.Objects;
 import java.util.Set;
-import java.util.StringJoiner;
 
 import org.apache.commons.lang3.SystemUtils;
 import org.slf4j.Logger;
@@ -49,8 +46,6 @@ import org.slf4j.LoggerFactory;
 import eu.albina.controller.ServerInstanceController;
 import eu.albina.model.AvalancheBulletin;
 import eu.albina.model.AvalancheBulletinDaytimeDescription;
-import eu.albina.model.AvalancheProblem;
-import eu.albina.model.enumerations.Aspect;
 import eu.albina.model.enumerations.DangerRating;
 import eu.albina.model.enumerations.LanguageCode;
 
@@ -246,88 +241,6 @@ public interface AlbinaUtil {
 		URL resource = AlbinaUtil.class.getClassLoader().getResource(name);
 		File file = new File(resource.getFile());
 		return URLDecoder.decode(file.getPath(), StandardCharsets.UTF_8);
-	}
-
-	static String getDangerRatingText(AvalancheBulletinDaytimeDescription daytimeBulletin, LanguageCode lang) {
-		String dangerRatingBelow;
-		String dangerRatingAbove;
-		if (daytimeBulletin.getDangerRatingBelow() == null || daytimeBulletin.getDangerRatingBelow().equals(DangerRating.missing) || daytimeBulletin.getDangerRatingBelow().equals(DangerRating.no_rating) || daytimeBulletin.getDangerRatingBelow().equals(DangerRating.no_snow)) {
-			dangerRatingBelow = DangerRating.no_rating.toString(lang.getLocale(), true);
-		} else {
-			dangerRatingBelow = daytimeBulletin.getDangerRatingBelow().toString(lang.getLocale(), true);
-		}
-		if (daytimeBulletin.getDangerRatingAbove() == null || daytimeBulletin.getDangerRatingAbove().equals(DangerRating.missing) || daytimeBulletin.getDangerRatingAbove().equals(DangerRating.no_rating) || daytimeBulletin.getDangerRatingAbove().equals(DangerRating.no_snow)) {
-			dangerRatingAbove = DangerRating.no_rating.toString(lang.getLocale(), true);
-		} else {
-			dangerRatingAbove = daytimeBulletin.getDangerRatingAbove().toString(lang.getLocale(), true);
-		}
-
-		if (daytimeBulletin.getTreeline()) {
-			return MessageFormat.format(lang.getBundleString("danger-rating.elevation"), dangerRatingBelow, lang.getBundleString("elevation.treeline"), dangerRatingAbove, lang.getBundleString("elevation.treeline"));
-		} else if (daytimeBulletin.getElevation() > 0) {
-			String elevation = daytimeBulletin.getElevation() + lang.getBundleString("unit.meter");
-			return MessageFormat.format(lang.getBundleString("danger-rating.elevation"), dangerRatingBelow, elevation, dangerRatingAbove, elevation);
-		} else {
-			return dangerRatingAbove;
-		}
-	}
-
-	static String getElevationString(AvalancheProblem avalancheProblem, LanguageCode lang) {
-		if (avalancheProblem.getTreelineHigh() || avalancheProblem.getElevationHigh() > 0) {
-			if (avalancheProblem.getTreelineLow() || avalancheProblem.getElevationLow() > 0) {
-				// elevation high and low set
-				String low = "";
-				String high = "";
-				if (avalancheProblem.getTreelineLow()) {
-					// elevation low treeline
-					low = lang.getBundleString("elevation.treeline");
-				} else if (avalancheProblem.getElevationLow() > 0) {
-					// elevation low number
-					low = avalancheProblem.getElevationLow() + lang.getBundleString("unit.meter");
-				}
-				if (avalancheProblem.getTreelineHigh()) {
-					// elevation high treeline
-					high = lang.getBundleString("elevation.treeline");
-				} else if (avalancheProblem.getElevationHigh() > 0) {
-					// elevation high number
-					high = avalancheProblem.getElevationHigh() + lang.getBundleString("unit.meter");
-				}
-				return MessageFormat.format(lang.getBundleString("elevation.band"), low, high);
-			} else {
-				// elevation high set
-				String high = "";
-				if (avalancheProblem.getTreelineHigh()) {
-					// elevation high treeline
-					high = lang.getBundleString("elevation.treeline");
-				} else if (avalancheProblem.getElevationHigh() > 0) {
-					// elevation high number
-					high = avalancheProblem.getElevationHigh() + lang.getBundleString("unit.meter");
-				}
-				return MessageFormat.format(lang.getBundleString("elevation.below"), high);
-			}
-		} else if (avalancheProblem.getTreelineLow() || avalancheProblem.getElevationLow() > 0) {
-			// elevation low set
-			String low = "";
-			if (avalancheProblem.getTreelineLow()) {
-				// elevation low treeline
-				low = lang.getBundleString("elevation.treeline");
-			} else if (avalancheProblem.getElevationLow() > 0) {
-				// elevation low number
-				low = avalancheProblem.getElevationLow() + lang.getBundleString("unit.meter");
-			}
-			return MessageFormat.format(lang.getBundleString("elevation.above"), low);
-		} else {
-			// no elevation set
-			return lang.getBundleString("elevation.all");
-		}
-	}
-
-	static String getAspectString(Set<Aspect> aspects, Locale locale) {
-		StringJoiner aspectString = new StringJoiner(", ");
-		for (Aspect aspect : aspects) {
-			aspectString.add(aspect.toString(locale));
-		}
-		return aspectString.toString();
 	}
 
 }
