3694f6f53731102f1b8507f63f4c3ffa09365cff
Norbert Lanzanasto <n.lanzanasto@gmail.com>
2025-02-21 12:12:44 +0100

    fix(danger-source): set snowHeightAverage default value to 0

--- src/main/resources/db/005_v5/005_000160_extend_danger_source_variants.sql
+++ src/main/resources/db/005_v5/005_000160_extend_danger_source_variants.sql
@@ -1,4 +1,4 @@
 -- liquibase formatted sql
 -- changeset albina:000_000050 failOnError:true
 
-ALTER TABLE danger_source_variants ADD SNOW_HEIGHT_AVERAGE integer;
+ALTER TABLE danger_source_variants ADD SNOW_HEIGHT_AVERAGE integer DEFAULT 0;
