9e1f33a03dc2f8dd1e20da0001c9ed548e4b3543
Christina Kirk <christina.kirk@proton.me>
2025-06-11 22:13:53 +0200

    chore(liquibase): v7 changesets

--- src/main/resources/db/007_v7/007_000100_whatsapp.sql
+++ src/main/resources/db/007_v7/007_000100_whatsapp.sql
@@ -11,6 +11,8 @@ CREATE TABLE whatsapp_configurations (
     CONSTRAINT FK_WHATSAPP_CONFIGURATIONS_REGIONS FOREIGN KEY (REGION_ID) REFERENCES regions (ID)
 );
 
+-- changeset christina:007_000101 failOnError:true
 ALTER TABLE avalanche_reports ADD COLUMN WHATSAPP_SENT bit(1) DEFAULT 0;
 
+-- changeset christina:007_000102 failOnError:true
 ALTER TABLE regions ADD COLUMN SEND_WHATSAPP_MESSAGES bit(1) DEFAULT 0;
---
+++ src/main/resources/db/007_v7/007_000220_danger_sources_foreign_keys.sql
@@ -0,0 +1,16 @@
+-- liquibase formatted sql
+
+-- changeset christina:1749672104307-8
+ALTER TABLE danger_source_variant_regions ADD CONSTRAINT FK3h4cnx2rft0jbndrnkhtq8onc FOREIGN KEY (DANGER_SOURCE_VARIANT_ID) REFERENCES danger_source_variants (ID);
+
+-- changeset christina:1749672104307-10
+ALTER TABLE danger_source_variant_terrain_types ADD CONSTRAINT FK4e7cd53qw8279cfmh061gl1lc FOREIGN KEY (DANGER_SOURCE_VARIANT_ID) REFERENCES danger_source_variants (ID);
+
+-- changeset christina:1749672104307-12
+ALTER TABLE danger_source_variant_aspects ADD CONSTRAINT FKfg2xat38uou4jpra0qfk6494 FOREIGN KEY (DANGER_SOURCE_VARIANT_ID) REFERENCES danger_source_variants (ID);
+
+-- changeset christina:1749672104307-14
+ALTER TABLE danger_source_variants ADD CONSTRAINT FKn8vbooq7hbxwxix8tnkfce1bl FOREIGN KEY (DANGER_SOURCE_ID) REFERENCES danger_sources (ID);
+
+-- changeset christina:1749672104307-15
+ALTER TABLE danger_source_variant_danger_signs ADD CONSTRAINT FKrwd0j6hkv0y5x8lx4ica2ge60 FOREIGN KEY (DANGER_SOURCE_VARIANT_ID) REFERENCES danger_source_variants (ID);
