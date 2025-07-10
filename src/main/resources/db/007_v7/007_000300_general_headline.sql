fdbd92d2ef1ce43431ce26fce234722afa482c50
Christoph Pippan <christoph@pippan.com>
2025-07-10 16:56:46 +0200

    fix(general-headline): update column type and default value for ENABLE_GENERAL_HEADLINE

--- src/main/resources/db/007_v7/007_000300_general_headline.sql
+++ src/main/resources/db/007_v7/007_000300_general_headline.sql
@@ -1,7 +1,7 @@
 -- liquibase formatted sql
 
 -- changeset albina:007_000300-1 failOnError:true
-ALTER TABLE regions ADD ENABLE_GENERAL_HEADLINE TINYINT(1) NULL;
+ALTER TABLE regions ADD ENABLE_GENERAL_HEADLINE bit(1) DEFAULT 0;
 
 -- changeset albina:007_000300-2 failOnError:true
 ALTER TABLE avalanche_bulletins ADD GENERAL_HEADLINE_COMMENT_NOTES LONGTEXT NULL;
