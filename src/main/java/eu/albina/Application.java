de7527e764a0d7cbb8e0a4744cd410b3d2bfc101
Simon Legner <Simon.Legner@gmail.com>
2025-08-21 09:29:45 +0200

    Application + micronaut.server.context-path=/albina/api

--- src/main/java/eu/albina/Application.java
+++ src/main/java/eu/albina/Application.java
@@ -4,30 +4,21 @@ import eu.albina.util.DBMigration;
 import eu.albina.util.GlobalVariables;
 import eu.albina.util.HibernateUtil;
 import eu.albina.util.SchedulerUtil;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.slf4j.bridge.SLF4JBridgeHandler;
 import io.micronaut.runtime.Micronaut;
-
-import java.sql.Driver;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.util.Enumeration;
 
 public class Application {
 
-	private static final Logger logger = LoggerFactory.getLogger(Application.class);
-
 	public static void main(String[] args) {
-//		GlobalVariables.loadConfigProperties();
-//
-//		SchedulerUtil.getInstance().setUp();
-//		SchedulerUtil.getInstance().start();
-		// TODO use micronaut way of schedulers by creating a Startup Listener Bean?
+		GlobalVariables.loadConfigProperties();
 
-		logger.debug("ServletContextListener started");
-		SLF4JBridgeHandler.removeHandlersForRootLogger();
-		SLF4JBridgeHandler.install();
+		HibernateUtil.getInstance().setUp();
+
+		SchedulerUtil.getInstance().setUp();
+		SchedulerUtil.getInstance().start();
+
+		DBMigration.executeMigration();
+		DBMigration.createAutoConfiguration();
+
 		Micronaut.run(Application.class, args);
 	}
 }
