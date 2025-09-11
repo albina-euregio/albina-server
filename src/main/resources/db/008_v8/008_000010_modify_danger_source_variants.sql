88e4d0d1139d9098eadadafc3aed53cb4cc0da4d
Norbert Lanzanasto <n.lanzanasto@gmail.com>
2025-09-11 15:49:04 +0200

    fix(sql): use correct table name

--- src/main/resources/db/008_v8/008_000010_modify_danger_source_variants.sql
+++ src/main/resources/db/008_v8/008_000010_modify_danger_source_variants.sql
@@ -1,12 +1,12 @@
 -- liquibase formatted sql
 -- changeset albina:008_000010 failOnError:true
 
-CREATE TABLE danger_source_variant_grain_shapes (
+CREATE TABLE danger_source_variant_weak_layer_grain_shapes (
     WEAK_LAYER_GRAIN_SHAPE varchar(255),
     DANGER_SOURCE_VARIANT_ID varchar(255) not null
 ) engine=InnoDB;
 
-INSERT INTO danger_source_variant_grain_shapes (WEAK_LAYER_GRAIN_SHAPE, DANGER_SOURCE_VARIANT_ID)
+INSERT INTO danger_source_variant_weak_layer_grain_shapes (WEAK_LAYER_GRAIN_SHAPE, DANGER_SOURCE_VARIANT_ID)
     SELECT WEAK_LAYER_GRAIN_SHAPE, ID
     FROM DANGER_SOURCE_VARIANTS
     WHERE WEAK_LAYER_GRAIN_SHAPE IS NOT NULL;
