4159783d021c9b4298c222b69565ae479273b59e
Simon Legner <Simon.Legner@gmail.com>
2025-10-15 20:35:08 +0200

    Convert to record

--- src/main/java/eu/albina/model/BulletinUpdate.java
+++ src/main/java/eu/albina/model/BulletinUpdate.java
@@ -7,42 +7,5 @@ import eu.albina.model.enumerations.BulletinStatus;
 import io.micronaut.serde.annotation.Serdeable;
 
 @Serdeable
-public class BulletinUpdate {
-
-	private String region;
-	private Instant date;
-	private BulletinStatus status;
-
-	public BulletinUpdate() {
-	}
-
-	public BulletinUpdate(String region, Instant date, BulletinStatus status) {
-		this.region = region;
-		this.date = date;
-		this.status = status;
-	}
-
-	public String getRegion() {
-		return region;
-	}
-
-	public void setRegion(String region) {
-		this.region = region;
-	}
-
-	public Instant getDate() {
-		return date;
-	}
-
-	public void setDate(Instant date) {
-		this.date = date;
-	}
-
-	public BulletinStatus getStatus() {
-		return status;
-	}
-
-	public void setStatus(BulletinStatus status) {
-		this.status = status;
-	}
+public record BulletinUpdate(String region, Instant date, BulletinStatus status) {
 }
