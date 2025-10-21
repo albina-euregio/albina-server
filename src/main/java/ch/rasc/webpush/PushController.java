744a9afae39000da4cbbd0eb517c74ba488bc9bb
Simon Legner <Simon.Legner@gmail.com>
2025-10-21 11:41:40 +0200

    Get rid of com.auth0.jwt dependency since micronaut uses nimbus-jose-jwt

--- src/main/java/ch/rasc/webpush/PushController.java
+++ src/main/java/ch/rasc/webpush/PushController.java
@@ -1,43 +1,53 @@
 package ch.rasc.webpush;
 
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.JWSAlgorithm;
+import com.nimbusds.jose.JWSHeader;
+import com.nimbusds.jose.JWSSigner;
+import com.nimbusds.jose.crypto.ECDSASigner;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.SignedJWT;
+
 import java.net.URI;
+import java.security.GeneralSecurityException;
+import java.security.interfaces.ECPrivateKey;
+import java.security.interfaces.ECPublicKey;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.algorithms.Algorithm;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 /**
  * Web push and signed JWTs
+ *
  * @apiNote https://developers.google.com/web/fundamentals/push-notifications/web-push-protocol#web_push_and_signed_jwts
  */
-public class PushController {
-
-	public static final Logger logger = LoggerFactory.getLogger(PushController.class);
-	private final ServerKeys serverKeys;
-
-	private final Algorithm jwtAlgorithm;
-
-	public PushController(ServerKeys serverKeys) {
-		this.serverKeys = serverKeys;
-		this.jwtAlgorithm = serverKeys.toJwtAlgorithm();
-	}
+public record PushController(ServerKeys serverKeys) {
 
 	private String getToken(String origin) {
 		Date expires = new Date(System.currentTimeMillis() + TimeUnit.HOURS.toMillis(12));
-		return JWT.create()
-			.withAudience(origin)
-			.withExpiresAt(expires)
-			.withSubject("mailto:example@example.com")
-			.sign(this.jwtAlgorithm);
+		JWTClaimsSet claims = new JWTClaimsSet.Builder()
+			.audience(origin)
+			.expirationTime(expires)
+			.subject("mailto:example@example.com")
+			.build();
+
+		try {
+			CryptoService cryptoService = new CryptoService();
+			ECPublicKey publicKey = cryptoService.fromUncompressedECPublicKey(serverKeys.publicKeyBase64());
+			ECPrivateKey privateKey = cryptoService.fromUncompressedECPrivateKey(serverKeys.privateKeyBase64(), publicKey);
+			JWSSigner signer = new ECDSASigner(privateKey);
+			JWSHeader header = new JWSHeader(JWSAlgorithm.ES256);
+			SignedJWT signedJWT = new SignedJWT(header, claims);
+			signedJWT.sign(signer);
+			return signedJWT.serialize();
+		} catch (JOSEException | GeneralSecurityException e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 	public String getAuthorization(URI endpointURI) {
 		String origin = endpointURI.getScheme() + "://" + endpointURI.getHost();
 		final String token = getToken(origin);
-		return "vapid t=" + token + ", k=" + this.serverKeys.getPublicKeyBase64();
+		return "vapid t=" + token + ", k=" + this.serverKeys.publicKeyBase64();
 	}
 
 }
--- src/main/java/ch/rasc/webpush/ServerKeys.java
+++ src/main/java/ch/rasc/webpush/ServerKeys.java
@@ -1,44 +1,9 @@
 package ch.rasc.webpush;
 
-import com.auth0.jwt.algorithms.Algorithm;
-
-import java.security.InvalidAlgorithmParameterException;
-import java.security.NoSuchAlgorithmException;
-import java.security.interfaces.ECPrivateKey;
-import java.security.interfaces.ECPublicKey;
-import java.security.spec.InvalidKeySpecException;
-
 /**
  * VAPID keys from https://vapidkeys.com/
  *
  * @apiNote https://developers.google.com/web/fundamentals/push-notifications/web-push-protocol#application_server_keys
  */
-public class ServerKeys {
-
-	private final String publicKeyBase64;
-	private final ECPublicKey publicKey;
-	private final ECPrivateKey privateKey;
-
-	public ServerKeys(String vapidPublicKey, String vapidPrivateKey) throws InvalidKeySpecException, InvalidAlgorithmParameterException, NoSuchAlgorithmException {
-		final CryptoService cryptoService = new CryptoService();
-		this.publicKeyBase64 = vapidPublicKey;
-		this.publicKey = cryptoService.fromUncompressedECPublicKey(vapidPublicKey);
-		this.privateKey = cryptoService.fromUncompressedECPrivateKey(vapidPrivateKey, publicKey);
-	}
-
-	public String getPublicKeyBase64() {
-		return this.publicKeyBase64;
-	}
-
-	public ECPrivateKey getPrivateKey() {
-		return this.privateKey;
-	}
-
-	public ECPublicKey getPublicKey() {
-		return this.publicKey;
-	}
-
-	public Algorithm toJwtAlgorithm() {
-		return Algorithm.ECDSA256(getPublicKey(), getPrivateKey());
-	}
+public record ServerKeys(String publicKeyBase64, String privateKeyBase64) {
 }
