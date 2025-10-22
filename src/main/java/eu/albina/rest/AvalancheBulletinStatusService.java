50b01dde226cb9771cc8438f7db519d8f4a40e98
Simon Legner <Simon.Legner@gmail.com>
2025-10-22 17:52:50 +0200

    AvalancheBulletinStatusService: no HttpResponse

--- src/main/java/eu/albina/rest/AvalancheBulletinStatusService.java
+++ src/main/java/eu/albina/rest/AvalancheBulletinStatusService.java
@@ -8,21 +8,18 @@ import eu.albina.model.AvalancheReport;
 import eu.albina.model.Region;
 import eu.albina.model.enumerations.BulletinStatus;
 import eu.albina.model.enumerations.Role;
-import io.micronaut.http.HttpResponse;
+import io.micronaut.http.HttpStatus;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.QueryValue;
+import io.micronaut.http.exceptions.HttpStatusException;
 import io.micronaut.security.annotation.Secured;
 import io.micronaut.serde.annotation.Serdeable;
 import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.media.ArraySchema;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.inject.Inject;
-import jakarta.persistence.PersistenceException;
+import jakarta.validation.constraints.NotBlank;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -31,7 +28,6 @@ import java.time.ZoneId;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.stream.Collectors;
 
 @Controller("/bulletins/status")
 @Tag(name = "bulletins/status")
@@ -50,8 +46,7 @@ public class AvalancheBulletinStatusService {
 	}
 
 	@Get
-	@ApiResponse(description = "status", content = @Content(array = @ArraySchema(schema = @Schema(implementation = Status.class))))
-	public HttpResponse<?> getStatus(@QueryValue("region") String regionId,
+	public List<Status> getStatus(@QueryValue("region") String regionId,
 									 @QueryValue("timezone") String timezone,
 									 @Parameter(description = DateControllerUtil.DATE_FORMAT_DESCRIPTION) @QueryValue("startDate") String start,
 									 @Parameter(description = DateControllerUtil.DATE_FORMAT_DESCRIPTION) @QueryValue("endDate") String end) {
@@ -69,76 +64,59 @@ public class AvalancheBulletinStatusService {
 				status = avalancheReportController.getStatus(startDate, endDate, regionRepository.findById(regionId).orElseThrow());
 			}
 
-			List<Status> result = status.entrySet().stream()
+			return status.entrySet().stream()
 				.map(entry -> new Status(entry.getKey(), entry.getValue(), null))
-				.collect(Collectors.toList());
-
-			return HttpResponse.ok(result);
-		} catch (AlbinaException e) {
+				.toList();
+		} catch (Exception e) {
 			logger.warn("Error loading status", e);
-			return HttpResponse.badRequest().body(e.toString());
+			throw new HttpStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
 		}
 	}
 
 	@Get("/internal")
 	@Secured({ Role.Str.ADMIN, Role.Str.FORECASTER, Role.Str.FOREMAN, Role.Str.OBSERVER })
 	@SecurityRequirement(name = AuthenticationService.SECURITY_SCHEME)
-	@ApiResponse(description = "status", content = @Content(array = @ArraySchema(schema = @Schema(implementation = Status.class))))
-	public HttpResponse<?> getInternalStatus(@QueryValue("region") String regionId,
-									  @Parameter(description = DateControllerUtil.DATE_FORMAT_DESCRIPTION) @QueryValue("startDate") String start,
-									  @Parameter(description = DateControllerUtil.DATE_FORMAT_DESCRIPTION) @QueryValue("endDate") String end) {
+	public List<Status> getInternalStatus(@QueryValue("region") String regionId,
+										  @Parameter(description = DateControllerUtil.DATE_FORMAT_DESCRIPTION) @QueryValue("startDate") String start,
+										  @Parameter(description = DateControllerUtil.DATE_FORMAT_DESCRIPTION) @QueryValue("endDate") String end) {
 
 		try {
 			Region region = regionRepository.findById(regionId).orElseThrow();
 			Instant startDate = DateControllerUtil.parseDateOrToday(start);
 			Instant endDate = DateControllerUtil.parseDateOrNull(end);
 
-			List<Status> result = avalancheReportController
+			return avalancheReportController
 				.getInternalStatus(startDate, endDate, region).entrySet().stream()
 				.map(entry -> new Status(entry.getKey(), entry.getValue(), null))
-				.collect(Collectors.toList());
-
-			return HttpResponse.ok(result);
-		} catch (AlbinaException e) {
-			logger.warn("Error loading status", e);
-			return HttpResponse.badRequest().body(e.toString());
-		} catch (PersistenceException e) {
-			logger.warn("Error loading status for " + regionId);
-			return HttpResponse.badRequest().body(e.toString());
+				.toList();
+		} catch (Exception e) {
+			logger.warn("Error loading status for " + regionId, e);
+			throw new HttpStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
 		}
 	}
 
 	@Get("/publications")
 	@Secured({ Role.Str.ADMIN, Role.Str.FORECASTER, Role.Str.FOREMAN, Role.Str.OBSERVER })
 	@SecurityRequirement(name = AuthenticationService.SECURITY_SCHEME)
-	@ApiResponse(description = "status", content = @Content(array = @ArraySchema(schema = @Schema(implementation = Status.class))))
-	public HttpResponse<?> getPublicationsStatus(@QueryValue("region") String regionId,
-										  @Parameter(description = DateControllerUtil.DATE_FORMAT_DESCRIPTION) @QueryValue("startDate") String start,
-										  @Parameter(description = DateControllerUtil.DATE_FORMAT_DESCRIPTION) @QueryValue("endDate") String end) {
+	public List<Status> getPublicationsStatus(@QueryValue("region") @NotBlank String regionId,
+											  @Parameter(description = DateControllerUtil.DATE_FORMAT_DESCRIPTION) @QueryValue("startDate") String start,
+											  @Parameter(description = DateControllerUtil.DATE_FORMAT_DESCRIPTION) @QueryValue("endDate") String end) {
 
 		Instant startDate = DateControllerUtil.parseDateOrToday(start);
 		Instant endDate = DateControllerUtil.parseDateOrNull(end);
 
-		if (regionId == null || regionId.isEmpty()) {
-			logger.warn("No region defined.");
-			return HttpResponse.noContent();
-		}
-
 		Region region = regionRepository.findById(regionId).orElseThrow();
-		List<Status> result = avalancheReportController
+		return avalancheReportController
 			.getPublicationStatus(startDate, endDate, region)
 			.entrySet().stream()
 			.map(entry -> new Status(entry.getKey(), null, entry.getValue()))
-			.collect(Collectors.toList());
-
-		return HttpResponse.ok(result);
+			.toList();
 	}
 
 	@Get("/publication")
 	@Secured({ Role.Str.ADMIN, Role.Str.FORECASTER, Role.Str.FOREMAN, Role.Str.OBSERVER })
 	@SecurityRequirement(name = AuthenticationService.SECURITY_SCHEME)
-	@ApiResponse(description = "status", content = @Content(schema = @Schema(implementation = Status.class)))
-	public HttpResponse<?> getPublicationStatus(@QueryValue("region") String regionId,
+	public Status getPublicationStatus(@QueryValue("region") String regionId,
 										 @Parameter(description = DateControllerUtil.DATE_FORMAT_DESCRIPTION) @QueryValue("date") String date) {
 
 		Instant startDate = DateControllerUtil.parseDateOrToday(date);
@@ -154,11 +132,10 @@ public class AvalancheBulletinStatusService {
 				throw new AlbinaException("No publication found!");
 
 			Entry<Instant, AvalancheReport> entry = status.entrySet().iterator().next();
-			Status result = new Status(entry.getKey(), null, entry.getValue());
-			return HttpResponse.ok(result);
-		} catch (AlbinaException e) {
+			return new Status(entry.getKey(), null, entry.getValue());
+		} catch (Exception e) {
 			logger.warn("Error loading status", e);
-			return HttpResponse.badRequest().body(e.toJSON());
+			throw new HttpStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
 		}
 	}
 }
