a23912a83a85f6098ebfa564b10f965303c624d2
Simon Legner <Simon.Legner@gmail.com>
2025-10-28 15:09:07 +0100

    refact: replace slf4j-simple with java.util.logging

--- src/main/java/eu/albina/Application.java
+++ src/main/java/eu/albina/Application.java
@@ -1,11 +1,30 @@
 package eu.albina;
 
+import com.google.common.io.Resources;
 import io.micronaut.runtime.Micronaut;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.logging.LogManager;
 
 public class Application {
 
-	public static void main(String[] args) {
+	private static void initLogging() throws IOException {
+		// Hibernate
 		System.setProperty("org.jboss.logging.provider", "slf4j");
+
+		// java.util.logging.Logger
+		if (System.getProperty("java.util.logging.config.file") != null) {
+			LogManager.getLogManager().readConfiguration();
+			return;
+		}
+		try (InputStream inputStream = Resources.getResource("logging.properties").openStream()) {
+			LogManager.getLogManager().readConfiguration(inputStream);
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		initLogging();
 		Micronaut.run(Application.class, args);
 	}
 }
