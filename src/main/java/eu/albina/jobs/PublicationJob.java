421fc892c7fbe4592c102fec6ae4cd0d4f4e35f5
Simon Legner <Simon.Legner@gmail.com>
2025-11-10 16:16:13 +0100

    Revert "fix(publication): commitTransaction before async"
    This reverts commit ab68b6f4717d6ed3a7b7df91e2ee643d9b9d54a9.

--- src/main/java/eu/albina/jobs/PublicationJob.java
+++ src/main/java/eu/albina/jobs/PublicationJob.java
@@ -16,8 +16,6 @@ import eu.albina.controller.RegionRepository;
 import eu.albina.controller.ServerInstanceRepository;
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -57,9 +55,6 @@ public class PublicationJob {
 
 	@Inject
 	ServerInstanceRepository serverInstanceRepository;
-
-	@PersistenceContext
-	EntityManager entityManager;
 
 	/**
 	 * Execute all necessary tasks to publish the bulletins at 5PM, depending
@@ -162,8 +157,6 @@ public class PublicationJob {
 			avalancheReport.setServerInstance(serverInstance);
 			return Stream.of(avalancheReport);
 		}).toList();
-
-		entityManager.getTransaction().commit();
 
 		// starting from here, everything should be run async in executor, method should return
 
