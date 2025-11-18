85538738aa2662a0fb644797b664832ce2456afa
Simon Legner <Simon.Legner@gmail.com>
2025-11-18 19:34:16 +0100

    fix: EmailUtilTest

--- src/test/java/eu/albina/util/EmailUtilTest.java
+++ src/test/java/eu/albina/util/EmailUtilTest.java
@@ -41,6 +41,7 @@ public class EmailUtilTest {
 		serverInstanceEuregio = new LocalServerInstance(false, false, "/mnt/bulletins/", null, "/mnt/simple_local/", null, null);
 		serverInstanceAran = new LocalServerInstance(false, false, "/mnt/albina_files_local/", null, "/mnt/simple_local/", null, null);
 		regionTyrol = regionTestUtils.regionTyrol();
+		regionTyrol.setServerImagesUrl("/mnt/images/");
 		regionAran = regionTestUtils.regionAran();
 	}
 
