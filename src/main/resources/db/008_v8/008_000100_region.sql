e153305ab9e143c1ee47e001f8c03c25a809c091
Simon Legner <Simon.Legner@gmail.com>
2025-11-18 19:40:26 +0100

    refact: LocalServerInstance (database migration)

---
+++ src/main/resources/db/008_v8/008_000100_region.sql
@@ -0,0 +1,11 @@
+-- liquibase formatted sql
+-- changeset albina:008_000100 failOnError:true
+
+ALTER TABLE regions
+DROP FOREIGN KEY IF EXISTS FKe74kalgqbt19lryn1tehx99r1;
+
+ALTER TABLE regions
+DROP COLUMN IF EXISTS SERVER_INSTANCE_ID;
+
+ALTER TABLE regions
+ADD COLUMN IF NOT EXISTS SERVER_IMAGES_URL varchar(191) COMMENT 'URL via which the static bulletin images can be retrieved. Used in the HTML bulletin and in e-mails.';
