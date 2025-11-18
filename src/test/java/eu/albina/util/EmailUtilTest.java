f37da6c723ae6becb5bac6056fff4627c4f83f7e
Simon Legner <Simon.Legner@gmail.com>
2025-11-18 17:42:48 +0100

    fix: EmailUtilTest

--- src/test/java/eu/albina/util/EmailUtilTest.java
+++ src/test/java/eu/albina/util/EmailUtilTest.java
@@ -38,8 +38,8 @@ public class EmailUtilTest {
 
 	@BeforeEach
 	public void setUp() throws IOException {
-		serverInstanceEuregio = new LocalServerInstance(false, false, "/mnt/bulletins/", null, null, "/mnt/simple_local/", null);
-		serverInstanceAran = new LocalServerInstance(false, false, "/mnt/albina_files_local/", null, null, "/mnt/simple_local/", null);
+		serverInstanceEuregio = new LocalServerInstance(false, false, "/mnt/bulletins/", null, "/mnt/simple_local/", null, null);
+		serverInstanceAran = new LocalServerInstance(false, false, "/mnt/albina_files_local/", null, "/mnt/simple_local/", null, null);
 		regionTyrol = regionTestUtils.regionTyrol();
 		regionAran = regionTestUtils.regionAran();
 	}
