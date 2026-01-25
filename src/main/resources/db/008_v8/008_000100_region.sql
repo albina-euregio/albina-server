b377bfe1c7fea24e84fd34299ec63113663db58a
Simon Legner <Simon.Legner@gmail.com>
2026-01-25 15:49:52 +0100

    chore: UPDATE regions SET SERVER_IMAGES_URL

--- src/main/resources/db/008_v8/008_000100_region.sql
+++ src/main/resources/db/008_v8/008_000100_region.sql
@@ -9,3 +9,7 @@ DROP COLUMN IF EXISTS SERVER_INSTANCE_ID;
 
 ALTER TABLE regions
 ADD COLUMN IF NOT EXISTS SERVER_IMAGES_URL varchar(191) COMMENT 'URL via which the static bulletin images can be retrieved. Used in the HTML bulletin and in e-mails.';
+
+UPDATE regions
+SET SERVER_IMAGES_URL = CONCAT(STATIC_URL, 'images/')
+WHERE SERVER_IMAGES_URL IS NULL;
