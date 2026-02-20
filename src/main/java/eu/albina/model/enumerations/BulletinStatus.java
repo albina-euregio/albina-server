fd2f6a12c89d6a2db8f731b473b9e9a8cc0dc44e
Simon Legner <Simon.Legner@gmail.com>
2026-02-20 17:16:34 +0100

    refact(BulletinStatus): simplify comparePublicationStatus

--- src/main/java/eu/albina/model/enumerations/BulletinStatus.java
+++ src/main/java/eu/albina/model/enumerations/BulletinStatus.java
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 package eu.albina.model.enumerations;
 
+import java.util.Comparator;
 import java.util.Set;
 
 import org.slf4j.Logger;
@@ -95,41 +96,17 @@ public enum BulletinStatus {
 		};
 	}
 
+	static final Comparator<BulletinStatus> COMPARATOR = Comparator.comparingInt(s -> switch (s) {
+		case missing -> 0;
+		case draft -> 1;
+		case updated -> 2;
+		case submitted -> 3;
+		case resubmitted -> 4;
+		case published -> 5;
+		case republished -> 6;
+	});
+
 	public int comparePublicationStatus(BulletinStatus status) {
-		return switch (this) {
-			case missing -> switch (status) {
-				case missing -> 0;
-				case draft, submitted, published, updated, resubmitted, republished -> -1;
-			};
-			case draft -> switch (status) {
-				case missing -> 1;
-				case draft -> 0;
-				case updated, submitted, published, resubmitted, republished -> -1;
-			};
-			case submitted -> switch (status) {
-				case missing, draft, updated -> 1;
-				case submitted -> 0;
-				case resubmitted, published, republished -> -1;
-			};
-			case published -> switch (status) {
-				case missing, draft, updated, submitted, resubmitted -> 1;
-				case published -> 0;
-				case republished -> -1;
-			};
-			case updated -> switch (status) {
-				case missing, draft -> 1;
-				case updated -> 0;
-				case submitted, published, resubmitted, republished -> -1;
-			};
-			case resubmitted -> switch (status) {
-				case missing, draft, updated, submitted -> 1;
-				case resubmitted -> 0;
-				case published, republished -> -1;
-			};
-			case republished -> switch (status) {
-				case missing, draft, updated, submitted, resubmitted, published -> 1;
-				case republished -> 0;
-			};
-		};
+		return COMPARATOR.compare(this, status);
 	}
 }
