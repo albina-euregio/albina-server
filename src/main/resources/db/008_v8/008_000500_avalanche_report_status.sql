b1f4c841d2d217b5f20f5f25a09e4c1bf525f741
Simon Legner <Simon.Legner@gmail.com>
2026-03-01 15:39:49 +0100

    refact(AvalancheReport): status as @Enumerated(EnumType.STRING)

---
+++ src/main/resources/db/008_v8/008_000500_avalanche_report_status.sql
@@ -0,0 +1,20 @@
+-- liquibase formatted sql
+-- changeset albina:008_000500 failOnError:true
+
+ALTER TABLE avalanche_reports
+    MODIFY COLUMN STATUS varchar(31);
+
+UPDATE avalanche_reports
+SET STATUS = CASE STATUS
+                 WHEN '0' THEN 'republished'
+                 WHEN '1' THEN 'resubmitted'
+                 WHEN '2' THEN 'updated'
+                 WHEN '3' THEN 'published'
+                 WHEN '4' THEN 'submitted'
+                 WHEN '5' THEN 'draft'
+                 WHEN '6' THEN 'missing'
+                 else null end;
+
+ALTER TABLE avalanche_reports
+    MODIFY COLUMN STATUS set ('republished', 'resubmitted', 'updated', 'published', 'submitted', 'draft', 'missing');
+
