743b9ddb4cfb3274958d5d8d0487299fa1dd64f9
Simon Legner <Simon.Legner@gmail.com>
2026-03-01 09:45:44 +0100

    refact: create eu.albina.model.GenericObservation

--- src/main/java/eu/albina/model/GenericObservation.java
+++ src/main/java/eu/albina/model/GenericObservation.java
@@ -1,11 +1,12 @@
 package eu.albina.model;
 
-import java.io.Serial;
-import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
+import java.util.Set;
 
+import io.micronaut.serde.annotation.Serdeable;
 import jakarta.persistence.Column;
+import jakarta.persistence.Convert;
 import jakarta.persistence.Embeddable;
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
@@ -16,311 +17,334 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
-import org.hibernate.annotations.ColumnDefault;
+
+import eu.albina.model.converter.EnumSetToStringConverter;
+import eu.albina.model.enumerations.Aspect;
+import eu.albina.model.enumerations.AvalancheProblem;
+import eu.albina.model.enumerations.DangerPattern;
 
 @Entity
 @Table(name = "generic_observations")
+@Serdeable
 public class GenericObservation {
+	@EmbeddedId
+	private GenericObservationId id;
 
-    @EmbeddedId
-    private GenericObservationId id;
+	@NotNull
+	@Column(name = "OBS_TYPE", nullable = false)
+	private ObservationType obsType;
 
-    @NotNull
-    @Column(name = "OBS_TYPE", nullable = false)
-    private String obsType;
+	@Column(name = "EXTERNAL_URL")
+	private String externalUrl;
 
-    @Column(name = "EXTERNAL_URL")
-    private String externalUrl;
+	@Column(name = "STABILITY")
+	private String stability;
 
-    @Column(name = "STABILITY")
-    private String stability;
+	@Column(name = "ASPECTS", columnDefinition = Aspect.Converter.COLUMN_DEFINITION)
+	@Convert(converter = Aspect.Converter.class)
+	private Set<Aspect> aspects;
 
-    @Column(name = "ASPECTS")
-    private String aspects;
+	@Column(name = "AUTHOR_NAME", length = 191)
+	private String authorName;
 
-    @Size(max = 191)
-    @Column(name = "AUTHOR_NAME", length = 191)
-    private String authorName;
+	@Column(name = "OBS_CONTENT")
+	private String obsContent;
 
-    @Column(name = "OBS_CONTENT")
-    private String obsContent;
+	@Column(name = "OBS_DATA")
+	private String obsData;
 
-    @Column(name = "OBS_DATA")
-    private String obsData;
+	@Column(name = "ELEVATION")
+	private Double elevation;
 
-    @Column(name = "ELEVATION")
-    private Double elevation;
+	@Column(name = "ELEVATION_LOWER_BOUND")
+	private Double elevationLowerBound;
 
-    @Column(name = "ELEVATION_LOWER_BOUND")
-    private Double elevationLowerBound;
+	@Column(name = "ELEVATION_UPPER_BOUND")
+	private Double elevationUpperBound;
 
-    @Column(name = "ELEVATION_UPPER_BOUND")
-    private Double elevationUpperBound;
+	@Column(name = "EVENT_DATE")
+	private Instant eventDate;
 
-    @Column(name = "EVENT_DATE")
-    private Instant eventDate;
+	@Column(name = "LATITUDE")
+	private Double latitude;
 
-    @Column(name = "LATITUDE")
-    private Double latitude;
+	@Column(name = "LOCATION_NAME", length = 191)
+	private String locationName;
 
-    @Size(max = 191)
-    @Column(name = "LOCATION_NAME", length = 191)
-    private String locationName;
+	@Column(name = "LONGITUDE")
+	private Double longitude;
 
-    @Column(name = "LONGITUDE")
-    private Double longitude;
+	@Column(name = "REGION_ID", length = 191)
+	private String regionId;
 
-    @Size(max = 191)
-    @Column(name = "REGION_ID", length = 191)
-    private String regionId;
+	@Column(name = "REPORT_DATE")
+	private Instant reportDate;
 
-    @Column(name = "REPORT_DATE")
-    private Instant reportDate;
+	@Column(name = "AVALANCHE_PROBLEMS", columnDefinition = AvalancheProblem.Converter.COLUMN_DEFINITION)
+	@Convert(converter = AvalancheProblem.Converter.class)
+	private Set<AvalancheProblem> avalancheProblems;
 
-    @Column(name = "AVALANCHE_PROBLEMS")
-    private String avalancheProblems;
+	@Column(name = "DANGER_PATTERNS", columnDefinition = DangerPattern.Converter.COLUMN_DEFINITION)
+	@Convert(converter = DangerPattern.Converter.class)
+	private Set<DangerPattern> dangerPatterns;
 
-    @Column(name = "DANGER_PATTERNS")
-    private String dangerPatterns;
+	@Column(name = "IMPORTANT_OBSERVATION", columnDefinition = ImportantObservation.Converter.COLUMN_DEFINITION)
+	@Convert(converter = ImportantObservation.Converter.class)
+	private Set<ImportantObservation> importantObservation;
 
-    @Column(name = "IMPORTANT_OBSERVATION")
-    private String importantObservation;
+	@Column(name = "EXTRA_DIALOG_ROWS")
+	private String extraDialogRows;
 
-    @Column(name = "EXTRA_DIALOG_ROWS")
-    private String extraDialogRows;
+	@Column(name = "EXTERNAL_IMG")
+	private String externalImg;
 
-    @Column(name = "EXTERNAL_IMG")
-    private String externalImg;
+	@Column(name = "PERSON_INVOLVEMENT")
+	@Enumerated(EnumType.STRING)
+	private PersonInvolvement personInvolvement;
 
-    @Column(name = "PERSON_INVOLVEMENT")
-    private String personInvolvement;
+	@Column(name = "DELETED")
+	private Boolean deleted;
 
-    @ColumnDefault("0")
-    @Column(name = "DELETED")
-    private Boolean deleted;
+	@Column(name = "ALLOW_EDIT")
+	private Boolean allowEdit;
 
-    @ColumnDefault("0")
-    @Column(name = "ALLOW_EDIT")
-    private Boolean allowEdit;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "DANGER_SOURCE")
+	private DangerSource dangerSource;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "DANGER_SOURCE")
-    private DangerSource dangerSource;
+	public GenericObservationId getId() {
+		return id;
+	}
 
-    public GenericObservationId getId() {
-        return id;
-    }
+	public void setId(GenericObservationId id) {
+		this.id = id;
+	}
 
-    public void setId(GenericObservationId id) {
-        this.id = id;
-    }
+	public ObservationType getObsType() {
+		return obsType;
+	}
 
-    public String getObsType() {
-        return obsType;
-    }
+	public void setObsType(ObservationType obsType) {
+		this.obsType = obsType;
+	}
 
-    public void setObsType(String obsType) {
-        this.obsType = obsType;
-    }
-
-    public String getExternalUrl() {
-        return externalUrl;
-    }
-
-    public void setExternalUrl(String externalUrl) {
-        this.externalUrl = externalUrl;
-    }
-
-    public String getStability() {
-        return stability;
-    }
-
-    public void setStability(String stability) {
-        this.stability = stability;
-    }
-
-    public String getAspects() {
-        return aspects;
-    }
-
-    public void setAspects(String aspects) {
-        this.aspects = aspects;
-    }
-
-    public String getAuthorName() {
-        return authorName;
-    }
-
-    public void setAuthorName(String authorName) {
-        this.authorName = authorName;
-    }
-
-    public String getObsContent() {
-        return obsContent;
-    }
-
-    public void setObsContent(String obsContent) {
-        this.obsContent = obsContent;
-    }
-
-    public String getObsData() {
-        return obsData;
-    }
-
-    public void setObsData(String obsData) {
-        this.obsData = obsData;
-    }
-
-    public Double getElevation() {
-        return elevation;
-    }
-
-    public void setElevation(Double elevation) {
-        this.elevation = elevation;
-    }
-
-    public Double getElevationLowerBound() {
-        return elevationLowerBound;
-    }
-
-    public void setElevationLowerBound(Double elevationLowerBound) {
-        this.elevationLowerBound = elevationLowerBound;
-    }
-
-    public Double getElevationUpperBound() {
-        return elevationUpperBound;
-    }
-
-    public void setElevationUpperBound(Double elevationUpperBound) {
-        this.elevationUpperBound = elevationUpperBound;
-    }
-
-    public Instant getEventDate() {
-        return eventDate;
-    }
-
-    public void setEventDate(Instant eventDate) {
-        this.eventDate = eventDate;
-    }
-
-    public Double getLatitude() {
-        return latitude;
-    }
-
-    public void setLatitude(Double latitude) {
-        this.latitude = latitude;
-    }
-
-    public String getLocationName() {
-        return locationName;
-    }
-
-    public void setLocationName(String locationName) {
-        this.locationName = locationName;
-    }
-
-    public Double getLongitude() {
-        return longitude;
-    }
-
-    public void setLongitude(Double longitude) {
-        this.longitude = longitude;
-    }
-
-    public String getRegionId() {
-        return regionId;
-    }
-
-    public void setRegionId(String regionId) {
-        this.regionId = regionId;
-    }
-
-    public Instant getReportDate() {
-        return reportDate;
-    }
-
-    public void setReportDate(Instant reportDate) {
-        this.reportDate = reportDate;
-    }
-
-    public String getAvalancheProblems() {
-        return avalancheProblems;
-    }
-
-    public void setAvalancheProblems(String avalancheProblems) {
-        this.avalancheProblems = avalancheProblems;
-    }
-
-    public String getDangerPatterns() {
-        return dangerPatterns;
-    }
-
-    public void setDangerPatterns(String dangerPatterns) {
-        this.dangerPatterns = dangerPatterns;
-    }
-
-    public String getImportantObservation() {
-        return importantObservation;
-    }
-
-    public void setImportantObservation(String importantObservation) {
-        this.importantObservation = importantObservation;
-    }
-
-    public String getExtraDialogRows() {
-        return extraDialogRows;
-    }
-
-    public void setExtraDialogRows(String extraDialogRows) {
-        this.extraDialogRows = extraDialogRows;
-    }
-
-    public String getExternalImg() {
-        return externalImg;
-    }
-
-    public void setExternalImg(String externalImg) {
-        this.externalImg = externalImg;
-    }
-
-    public String getPersonInvolvement() {
-        return personInvolvement;
-    }
-
-    public void setPersonInvolvement(String personInvolvement) {
-        this.personInvolvement = personInvolvement;
-    }
-
-    public Boolean getDeleted() {
-        return deleted;
-    }
-
-    public void setDeleted(Boolean deleted) {
-        this.deleted = deleted;
-    }
-
-    public Boolean getAllowEdit() {
-        return allowEdit;
-    }
-
-    public void setAllowEdit(Boolean allowEdit) {
-        this.allowEdit = allowEdit;
-    }
-
-    public DangerSource getDangerSource() {
-        return dangerSource;
-    }
-
-    public void setDangerSource(DangerSource dangerSource) {
-        this.dangerSource = dangerSource;
-    }
+	public String getExternalUrl() {
+		return externalUrl;
+	}
+
+	public void setExternalUrl(String externalUrl) {
+		this.externalUrl = externalUrl;
+	}
+
+	public String getStability() {
+		return stability;
+	}
+
+	public void setStability(String stability) {
+		this.stability = stability;
+	}
+
+	public Set<Aspect> getAspects() {
+		return aspects;
+	}
+
+	public void setAspects(Set<Aspect> aspects) {
+		this.aspects = aspects;
+	}
+
+	public String getAuthorName() {
+		return authorName;
+	}
+
+	public void setAuthorName(String authorName) {
+		this.authorName = authorName;
+	}
+
+	public String getObsContent() {
+		return obsContent;
+	}
+
+	public void setObsContent(String obsContent) {
+		this.obsContent = obsContent;
+	}
+
+	public String getObsData() {
+		return obsData;
+	}
+
+	public void setObsData(String obsData) {
+		this.obsData = obsData;
+	}
+
+	public Double getElevation() {
+		return elevation;
+	}
+
+	public void setElevation(Double elevation) {
+		this.elevation = elevation;
+	}
+
+	public Double getElevationLowerBound() {
+		return elevationLowerBound;
+	}
+
+	public void setElevationLowerBound(Double elevationLowerBound) {
+		this.elevationLowerBound = elevationLowerBound;
+	}
+
+	public Double getElevationUpperBound() {
+		return elevationUpperBound;
+	}
+
+	public void setElevationUpperBound(Double elevationUpperBound) {
+		this.elevationUpperBound = elevationUpperBound;
+	}
+
+	public Instant getEventDate() {
+		return eventDate;
+	}
+
+	public void setEventDate(Instant eventDate) {
+		this.eventDate = eventDate;
+	}
+
+	public Double getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(Double latitude) {
+		this.latitude = latitude;
+	}
+
+	public String getLocationName() {
+		return locationName;
+	}
+
+	public void setLocationName(String locationName) {
+		this.locationName = locationName;
+	}
+
+	public Double getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(Double longitude) {
+		this.longitude = longitude;
+	}
+
+	public String getRegionId() {
+		return regionId;
+	}
+
+	public void setRegionId(String regionId) {
+		this.regionId = regionId;
+	}
+
+	public Instant getReportDate() {
+		return reportDate;
+	}
+
+	public void setReportDate(Instant reportDate) {
+		this.reportDate = reportDate;
+	}
+
+	public Set<AvalancheProblem> getAvalancheProblems() {
+		return avalancheProblems;
+	}
+
+	public void setAvalancheProblems(Set<AvalancheProblem> avalancheProblems) {
+		this.avalancheProblems = avalancheProblems;
+	}
+
+	public Set<DangerPattern> getDangerPatterns() {
+		return dangerPatterns;
+	}
+
+	public void setDangerPatterns(Set<DangerPattern> dangerPatterns) {
+		this.dangerPatterns = dangerPatterns;
+	}
+
+	public Set<ImportantObservation> getImportantObservation() {
+		return importantObservation;
+	}
+
+	public void setImportantObservation(Set<ImportantObservation> importantObservation) {
+		this.importantObservation = importantObservation;
+	}
+
+	public String getExtraDialogRows() {
+		return extraDialogRows;
+	}
+
+	public void setExtraDialogRows(String extraDialogRows) {
+		this.extraDialogRows = extraDialogRows;
+	}
+
+	public String getExternalImg() {
+		return externalImg;
+	}
+
+	public void setExternalImg(String externalImg) {
+		this.externalImg = externalImg;
+	}
+
+	public PersonInvolvement getPersonInvolvement() {
+		return personInvolvement;
+	}
+
+	public void setPersonInvolvement(PersonInvolvement personInvolvement) {
+		this.personInvolvement = personInvolvement;
+	}
+
+	public Boolean getDeleted() {
+		return deleted;
+	}
+
+	public void setDeleted(Boolean deleted) {
+		this.deleted = deleted;
+	}
+
+	public Boolean getAllowEdit() {
+		return allowEdit;
+	}
+
+	public void setAllowEdit(Boolean allowEdit) {
+		this.allowEdit = allowEdit;
+	}
+
+	public DangerSource getDangerSource() {
+		return dangerSource;
+	}
+
+	public void setDangerSource(DangerSource dangerSource) {
+		this.dangerSource = dangerSource;
+	}
+
+	public enum ObservationType {
+		SimpleObservation, Evaluation, Avalanche, Blasting, Closure, Profile, TimeSeries, Webcam, DrySnowfallLevel
+	}
+
+	public enum PersonInvolvement {
+		Dead, Injured, Uninjured, No, Unknown
+	}
+
+	public enum ImportantObservation {
+		SnowLine, SurfaceHoar, Graupel, StabilityTest, IceFormation, VeryLightNewSnow, ForBlog;
+
+		@jakarta.persistence.Converter
+		public static class Converter extends EnumSetToStringConverter<ImportantObservation> {
+
+			public static final String COLUMN_DEFINITION = "set('SnowLine',.'SurfaceHoar',.'Graupel',.'StabilityTest',.'IceFormation',.'VeryLightNewSnow',.'ForBlog')";
+
+			@Override
+			protected Class<ImportantObservation> getEnumClass() {
+				return ImportantObservation.class;
+			}
+		}
+	}
 
 	@Embeddable
-	public static class GenericObservationId implements Serializable {
-		@Serial
-		private static final long serialVersionUID = -1262080281652170193L;
-
+	public static class GenericObservationId {
 		@NotNull
 		@Column(name = "SOURCE", nullable = false, length = 191)
 		private String source;
@@ -328,6 +352,14 @@ public class GenericObservation {
 		@NotNull
 		@Column(name = "ID", nullable = false, length = 191)
 		private String id;
+
+		public GenericObservationId() {
+		}
+
+		public GenericObservationId(String source, String id) {
+			this.source = source;
+			this.id = id;
+		}
 
 		public String getSource() {
 			return source;
@@ -351,7 +383,7 @@ public class GenericObservation {
 			if (o == null || getClass() != o.getClass()) return false;
 			GenericObservationId entity = (GenericObservationId) o;
 			return Objects.equals(this.source, entity.source) &&
-					Objects.equals(this.id, entity.id);
+				Objects.equals(this.id, entity.id);
 		}
 
 		@Override
