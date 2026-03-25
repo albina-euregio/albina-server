b6bd14f098d828fa3ba0e3024d1cbe2d9a5010f0
Simon Legner <Simon.Legner@gmail.com>
2026-03-25 15:38:33 +0100

    chore(blog): try `@Introspected`

--- src/main/java/eu/albina/controller/publication/blog/BlogItem.java
+++ src/main/java/eu/albina/controller/publication/blog/BlogItem.java
@@ -5,15 +5,24 @@ import eu.albina.model.publication.BlogConfiguration;
 
 import java.time.OffsetDateTime;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import io.micronaut.core.annotation.Introspected;
+
+@Introspected
 public interface BlogItem {
+	@JsonInclude
 	String getId();
 
+	@JsonInclude
 	String getTitle();
 
+	@JsonInclude
 	String getContent();
 
+	@JsonInclude
 	OffsetDateTime getPublished();
 
+	@JsonInclude
 	String getAttachmentUrl();
 
 	default String getTitleAndUrl(BlogConfiguration config) {
